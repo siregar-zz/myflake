2a155badc5a5ff681b1477b9478271ad4342f9db
Roma <romadisiregar@jogjacamp.com>
2026-01-09 22:57:05 +0700

    tambah minipc aardvark

---
+++ hosts/aardvark/configuration.nix
@@ -0,0 +1,57 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "aardvark";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Asia/Jakarta";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  services.xserver = {
+    enable = true;
+
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+
+    xkb = {
+      layout = "us";
+      variant = "";
+    };
+  };
+
+  users.users.aardvark = {
+    isNormalUser = true;
+    description = "aardvark";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    alacritty
+    firefox
+    feh
+    networkmanagerapplet
+    xclip
+  ];
+
+  services.openssh.enable = true;
+
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+  system.stateVersion = "25.11";
+}
